c29d2102201bcd9a1d1eaa6e8183d00ab2ec9b6e
JerloPH <jewelcarlosalvador@gmail.com>
2021-04-18 09:13:10 +0800

    NEW: Class to store global functions, used anywhere

---
+++ Shelf/Functions/GlobalFunc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelf.Functions
+{
+    public static class GlobalFunc
+    {
+        #region File IO
+        public static string ReadFromFile(string filename)
+        {
+            string content = String.Empty;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            catch { }
+            return content;
+        }
+        #endregion
+    }
+}

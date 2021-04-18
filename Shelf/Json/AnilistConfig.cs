384fe29e831bdb7dc3c77a5707bde015cae48879
JerloPH <jewelcarlosalvador@gmail.com>
2021-04-18 09:14:00 +0800

    NEW: Add AnilistConfig.json

---
+++ Shelf/Json/AnilistConfig.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Shelf.Json
+{
+    public class AnilistConfig
+    {
+        [JsonProperty("clientId")]
+        public string clientId { get; set; } = "";
+        [JsonProperty("clientSecret")]
+        public string clientSecret { get; set; } = "";
+    }
+}
--- Shelf/frmGetAccessTkn.cs
+++ Shelf/frmGetAccessTkn.cs
@@ -8,24 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
+using Shelf.Functions;
 
 namespace Shelf
 {
     public partial class frmGetAccessTkn : Form
     {
         public string accessToken { get; set; } = "";
+        private string ANICLIENT = "";
+        private string ANISECRET = "";
         private string redirect_uri = @"https://anilist.co/api/v2/oauth/pin";
         private string AnilistUrl = "";
         public frmGetAccessTkn()
         {
             InitializeComponent();
+            // Initialize vars
             btnOK.DialogResult = DialogResult.OK;
             AnilistUrl = $"https://anilist.co/api/v2/oauth/authorize?client_id={ANICLIENT}&redirect_uri={redirect_uri}&response_type=code";
-            MessageBox.Show(AnilistUrl);
+            try
+            {
+                string content = GlobalFunc.ReadFromFile("");
+                var jsonConfig = JsonConvert.DeserializeObject<>(content);
+            }
+            catch { }
+            // MessageBox.Show(AnilistUrl);
+            InitializeAsync();
+        }
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
         }
         private async Task NavigateTo(string url)
         {
-            await webView.EnsureCoreWebView2Async();
+            //await webView.EnsureCoreWebView2Async();
             if (webView != null && webView.CoreWebView2 != null)
             {
                 webView.Source = new Uri(url);

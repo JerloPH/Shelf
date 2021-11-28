30b5fc3b0f415e634a3e6a68ab50ccf640ac31ee
JerloPH <jewelcarlosalvador@gmail.com>
2021-11-28 13:55:17 +0800

    Add icon to executable file.

--- Shelf/Properties/Resources.Designer.cs
+++ Shelf/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Shelf.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon MainAppIcon {
+            get {
+                object obj = ResourceManager.GetObject("MainAppIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap nocover {
